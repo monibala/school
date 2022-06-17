4d7b6538665a5506592bd14c63f663189c090c8c
Monika <Monika.cyberflax@gmail.com>
2022-06-17 13:22:28 +0530

    some change

--- p1/settings.py
+++ p1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p^*jc3$c!zh4t7*)*ylv5#xnr#^%c%m6x(ep)1q588xi#1sevf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
