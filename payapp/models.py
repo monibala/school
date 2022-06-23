bda6f67c5e085b67e07f6b9768cb0640ca1814cf
Monika <Monika.cyberflax@gmail.com>
2022-06-23 13:12:25 +0530

    some changes

--- payapp/models.py
+++ payapp/models.py
@@ -12,7 +12,7 @@ class Transaction(models.Model):
     made_on = models.DateTimeField(auto_now_add=True)
     amount = models.IntegerField()
     order_id = models.CharField(unique=True, max_length=100, null=True, blank=True)
-    checksum = models.CharField(max_length=100, null=True, blank=True)
+    checksum = models.CharField(max_length=1000, null=True, blank=True)
 
     # def save(self, *args, **kwargs):
     #     if self.order_id is None and self.made_on and self.id:
